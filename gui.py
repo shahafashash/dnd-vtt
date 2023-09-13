f36666d79b753fea1b9461ae188fef7253a982be
simon Lab <simelav24@gmail.com>
2023-09-13 22:44:45 +0300

    font class

--- gui.py
+++ gui.py
@@ -2,10 +2,15 @@ import pygame
 from enum import Enum
 from typing import Any
 
+def draw_rect(surface, color, rect):
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    surface.blit(shape_surf, rect)
 
 class GUI:
     win = None
     font = None
+    font2 = None
 
     elements = []
     focused_element = None
@@ -44,6 +49,7 @@ class Button:
     def __init__(self, text, key):
         self.text = text
         self.surf = GUI.font.render(text, True, (0, 0, 0))
+        self.surf_selected = GUI.font2.render(text, True, (0, 0, 0))
         self.key = key
 
         self.pos = (0, 0)
@@ -62,12 +68,15 @@ class Button:
             GUI.focused_element = self
 
     def draw(self):
-        pygame.draw.rect(
-            GUI.win,
-            (255, 255, 255),
-            (self.pos[0], self.pos[1], self.size[0], self.size[1]),
-        )
-        GUI.win.blit(self.surf, self.pos)
+        # draw_rect(
+            # GUI.win,
+            # (0, 0, 0, 100),
+            # (self.pos[0], self.pos[1], self.size[0], self.size[1])
+        # )
+        if self is GUI.focused_element:
+            GUI.win.blit(self.surf_selected, (self.pos[0] + self.size[0] // 2 - self.surf.get_width() // 2, self.pos[1]))
+        else:
+            GUI.win.blit(self.surf, (self.pos[0] + self.size[0] // 2 - self.surf.get_width() // 2, self.pos[1]))
 
     def click(self):
         self.parent.click()
@@ -77,14 +86,16 @@ class ContextMenu:
     def __init__(self, pos):
         self.pos = pos
         self.elements = []
+        self.width = 0
 
     def add_button(self, text, key):
         button = Button(text, key)
         button.parent = self
         self.elements.append(button)
-
+        self.width = max(self.width, button.size[0])
         for i, element in enumerate(self.elements):
             element.pos = (self.pos[0], self.pos[1] + i * element.size[1])
+            element.size = (self.width, element.size[1])
 
     def click(self):
         GUI.elements.remove(self)
