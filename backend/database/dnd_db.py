cd4c216fa7fa95c0279eba635226d1fba05d7ae5
Shahaf Ashash <shahaf.ashash@icloud.com>
2023-11-15 19:40:47 +0200

    removed redundent format

--- backend/database/dnd_db.py
+++ backend/database/dnd_db.py
@@ -9,8 +9,5 @@ load_dotenv()
 class DndDatabase(Database):
     def __init__(self) -> None:
         connection_string = os.getenv("MONGO_CONNECTION_STRING")
-        connection_string = connection_string.format(
-            username=username, password=password
-        )
         database_name = os.getenv("MONGO_DATABASE_NAME")
         super().__init__(connection_string, database_name)
